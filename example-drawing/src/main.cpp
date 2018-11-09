f43d4bbe7a51dc3abd3f53f4a1bc4f96193ce212
watab0shi <wtbs.kani@gmail.com>
2018-11-09 17:56:16 +0900

    Fix typo

--- example-drawing/src/main.cpp
+++ example-drawing/src/main.cpp
@@ -5,7 +5,7 @@
 int main( ){
 	ofGLWindowSettings settings;
 	settings.setGLVersion(4, 1);
-	settings.setSizee(1280, 720);
+	settings.setSize(1280, 720);
 	ofCreateWindow(settings);
 	ofRunApp(new ofApp());
 }
